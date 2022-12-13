d034c978299422f86d410265378de4b4ca4a4c4a
Laurens256 <duinlaurens2@gmail.com>
2022-12-13 17:51:48 +0100

    xml error handling

--- scripts/script.ts
+++ scripts/script.ts
@@ -11,15 +11,22 @@ req.overrideMimeType('text/xml');
 req.onload = () => {
 	if (req.readyState === req.DONE && req.status === 200) {
 		console.log(req.response);
-		getOnlineFilePercentage(req.response);
+		if(req.response.getElementsByTagName('error').length > 0) {
+			req.onerror!(req.response);
+			return;
+		}
+		getOnlineFileFraction(req.response);
 	}
 };
 
-let onlineFileCount = 0;
-let allFiles: Element[] = [];
-const getOnlineFilePercentage = (xml: XMLDocument) => {
+req.onerror = () => {
+	document.querySelector('main.visualisation-container')!.classList.add('data-error');
+};
+
+const getOnlineFileFraction = (xml: XMLDocument) => {
+	let onlineFileCount: number = 0;
 	// haalt alle bestanden op die een level hebben van 'file', zorgt ervoor dat kopjes en dergelijke niet worden meegenomen (nog navragen)
-	allFiles = Array.from(xml.getElementsByTagName('c')).filter(file => file.getAttribute('level') === 'file');
+	const allFiles: Element[] = Array.from(xml.getElementsByTagName('c')).filter(file => file.getAttribute('level') === 'file');
 	console.log(allFiles);
 	allFiles.forEach(file => {
 		const dao = file.getElementsByTagName('dao');
@@ -27,12 +34,12 @@ const getOnlineFilePercentage = (xml: XMLDocument) => {
 			onlineFileCount++;
 		}
 	});
-
-	const percentage: number = parseFloat((onlineFileCount / allFiles.length * 100).toFixed(2));
-	showVisualisation(percentage);
+	showVisualisation(onlineFileCount, allFiles);
 }
 
-const showVisualisation = (percentage: number) => {
+const showVisualisation = (onlineFileCount: number, allFiles: Element[]) => {
+	const percentage: number = parseFloat((onlineFileCount / allFiles.length * 100).toFixed(2));
+
 	const mainElement: HTMLElement = document.querySelector('main.visualisation-container')!;
 	const bar: HTMLElement = document.querySelector('.bar')!;
 	const percentageText: HTMLElement = document.querySelector('.percentage-text')!;
