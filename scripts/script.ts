8913ce5877ed9d5fc32a04abdf1e301b9fcdfea1
Laurens256 <duinlaurens2@gmail.com>
2022-12-13 17:29:58 +0100

    add bar visualisation

--- scripts/script.ts
+++ scripts/script.ts
@@ -1,3 +1,6 @@
+// dit zorgt voor een flash van unstyled content dus doen we niet
+// import "../styles/style.css"
+
 const req = new XMLHttpRequest;
 req.open('GET', 'https://service.archief.nl/gaf/oai/!open_oai.OAIHandler?verb=ListRecords&set=2.01.27.01&metadataPrefix=oai_ead');
 // req.open('GET', 'https://service.archief.nl/gaf/oai/!open_oai.OAIHandler?verb=ListRecords&set=2.05.170&metadataPrefix=oai_ead');
@@ -8,21 +11,39 @@ req.overrideMimeType('text/xml');
 req.onload = () => {
 	if (req.readyState === req.DONE && req.status === 200) {
 		console.log(req.response);
-		getOnlineFilePercent(req.response);
+		getOnlineFilePercentage(req.response);
 	}
 };
 
-const getOnlineFilePercent = (xml: XMLDocument) => {
-	const allFiles = Array.from(xml.getElementsByTagName('c')).filter(file => file.getAttribute('level') === 'file');
+let onlineFileCount = 0;
+let allFiles: Element[] = [];
+const getOnlineFilePercentage = (xml: XMLDocument) => {
+	// haalt alle bestanden op die een level hebben van 'file', zorgt ervoor dat kopjes en dergelijke niet worden meegenomen (nog navragen)
+	allFiles = Array.from(xml.getElementsByTagName('c')).filter(file => file.getAttribute('level') === 'file');
 	console.log(allFiles);
-	let onlineFileCount = 0;
 	allFiles.forEach(file => {
 		const dao = file.getElementsByTagName('dao');
 		if (dao.length > 0) {
 			onlineFileCount++;
 		}
 	});
-	console.log(Math.round(onlineFileCount / allFiles.length * 100));
+
+	const percentage: number = parseFloat((onlineFileCount / allFiles.length * 100).toFixed(2));
+	showVisualisation(percentage);
 }
+
+const showVisualisation = (percentage: number) => {
+	const mainElement: HTMLElement = document.querySelector('main.visualisation-container')!;
+	const bar: HTMLElement = document.querySelector('.bar')!;
+	const percentageText: HTMLElement = document.querySelector('.percentage-text')!;
+	const fullFraction: HTMLElement = document.querySelector('.full-fraction')!;
+
+	if (bar && percentageText && mainElement && fullFraction) {
+		bar.style.width = percentage + '%';
+		percentageText.textContent = percentage.toLocaleString() + '%';
+		fullFraction.textContent = `(${onlineFileCount} / ${allFiles.length})`;
+		mainElement.classList.add('data-loaded');
+	}
+};
 
 req.send();
